d0dc00b9216527236e39a9d432b8dc569a3ac078
Anoop Kumar Sharma <work4you114@gmail.com>
2021-11-06 03:46:38 +0530

    make changes as asked

--- SwardMan/Assets/Scripts/Player/Movement.cs
+++ SwardMan/Assets/Scripts/Player/Movement.cs
@@ -3,21 +3,16 @@ using System;
 public class Movement : MonoBehaviour
 {
     [Header("Refrance")]
-    public Camera mainCam;
+    public SwipeControl swipe;
     public GameObject playerBody;
     public Animator palyerAnimator;
 
     [Header("Change Values")]
-    public float speed = 0.25f;
-    public float swipeSpeed = 10f;
-    public float dragSpeed = 5f;
+    public float speed;
 
     //private
     private Transform localTransform;
     private Transform playerTransform;
-    private Vector3 lastMousePos;
-    private Vector3 mousePos;
-    private Vector3 newMousePos;
 
     private void Start()
     {
@@ -26,30 +21,55 @@ public class Movement : MonoBehaviour
     }
     void FixedUpdate()
     {
-        if (Input.GetMouseButton(0))
+        if(swipe.SwipeLeft)
         {
-            MovePlayer();
-            palyerAnimator.SetBool("run", true);
+            Debug.Log("left");
+            MovePlayerLeft();
+            // palyerAnimator.SetBool("attack",false);
+        }
+        else if(swipe.SwipeRight)
+        {
+            Debug.Log("right");
+            MovePlayerRight();
+            // palyerAnimator.SetBool("attack",false);
+        }
+        else if(swipe.SwipeDown)
+        {
+            Debug.Log("down");
+            MovePlayerBackword();
+            palyerAnimator.SetBool("back",true);
+        }
+        else if(swipe.SwipeUp)
+        {
+            Debug.Log("up");
+            MovePlayerForward();
+            palyerAnimator.SetBool("jump",true);
         }
         else
         {
-            palyerAnimator.SetBool("run", false);
+            // playerTransform.transform.localRotation = Quaternion.Euler(0, 0, 0);
         }
-        playerTransform.transform.localRotation = Quaternion.Euler(0, 0, 0);
-    }
-
-    private void MovePlayer()
-    {
-        mousePos = mainCam.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, dragSpeed));
-
-        float xDifference = mousePos.x - lastMousePos.x;
-        newMousePos.x = localTransform.position.x + xDifference * swipeSpeed * Time.deltaTime;
-        newMousePos.x = Mathf.Clamp(newMousePos.x, -0.4f, 0.4f);
-        newMousePos.y = localTransform.position.y;
-        newMousePos.z = localTransform.position.z;
-
-        localTransform.position = newMousePos + localTransform.forward * speed * Time.deltaTime;
         
-        lastMousePos = mousePos;
     }
+
+    private void MovePlayerRight()
+    { 
+        playerTransform.position = Vector3.MoveTowards(playerTransform.position,Vector3.right,speed*Time.deltaTime);
+    }
+    private void MovePlayerLeft()
+    { 
+        playerTransform.position = Vector3.MoveTowards(playerTransform.position,Vector3.left,speed*Time.deltaTime);
+    }
+    private void MovePlayerForward()
+    {
+        playerTransform.position = Vector3.MoveTowards(playerTransform.position,Vector3.forward,speed*Time.deltaTime);
+        
+        palyerAnimator.SetBool("jump",false);
+    }
+    private void MovePlayerBackword()
+    { 
+        playerTransform.position = Vector3.MoveTowards(playerTransform.position,Vector3.back,speed*Time.deltaTime);
+        palyerAnimator.SetBool("back",false);
+    }
+    
 }
---
+++ SwardMan/Assets/Scripts/Player/SwipeControl.cs
@@ -0,0 +1,133 @@
+using UnityEngine;
+
+public class SwipeControl : MonoBehaviour
+{
+    private bool swipeLeft, swipeRight, swipeUp, swipeDown;
+    private bool isDraging = false;
+    private Vector2 startTouch, swipeDelta;
+
+    private void Update() 
+    {
+        swipeDown = swipeLeft = swipeRight = swipeUp = false;
+
+        #region Standalone Inputs
+        if(Input.GetMouseButtonDown(0))
+        {
+            isDraging = true;
+            startTouch = Input.mousePosition;
+        }
+        else if(Input.GetMouseButtonUp(0))
+        {
+            isDraging = false;
+            Reset();
+        }
+        #endregion
+
+        #region Mobile Input
+        if(Input.touches.Length > 0)
+        {
+            if(Input.touches[0].phase == TouchPhase.Began)
+            {
+                isDraging = true;
+                startTouch = Input.touches[0].position;
+            }
+            else if(Input.touches[0].phase == TouchPhase.Ended || Input.touches[0].phase == TouchPhase.Canceled)
+            {
+                isDraging = false;
+                Reset();
+            }
+        }
+        #endregion
+
+        //Calculate Distance 
+        swipeDelta = Vector2.zero;
+        if(isDraging)
+        {
+            if(Input.touches.Length > 0)
+            {
+                swipeDelta = Input.touches[0].position - startTouch;
+            }
+            else if(Input.GetMouseButton(0))
+            {
+                swipeDelta = (Vector2)Input.mousePosition - startTouch;
+            }
+        }
+
+        // finding the direction(up,down,left,right)
+        if(swipeDelta.magnitude > 20)
+        {
+            //Direction??
+            float x = swipeDelta.x;
+            float y = swipeDelta.y;
+            if(Mathf.Abs(x) > Mathf.Abs(y))
+            {
+                // left or right
+                if(x < 0)
+                {
+                    swipeLeft = true;
+                }
+                else
+                {
+                    swipeRight = true;
+                }
+            }
+            else
+            {
+                // up or down
+                if(y < 0)
+                {
+                    swipeDown = true;
+                }
+                else
+                {
+                    swipeUp = true;
+                }
+            }
+
+            Reset();
+        }
+    }
+
+    private void Reset() 
+    {
+        startTouch = swipeDelta = Vector2.zero;
+        isDraging = false;
+    }
+#region Returns
+    public Vector2 SwipeDelta
+    {
+        get
+        {
+            return swipeDelta;
+        }
+    }
+    public bool SwipeLeft
+    {
+        get
+        {
+            return swipeLeft;
+        }
+    }
+    public bool SwipeRight
+    {
+        get
+        {
+            return swipeRight;
+        }
+    }
+    public bool SwipeUp
+    {
+        get
+        {
+            return swipeUp;
+        }
+    }
+    public bool SwipeDown
+    {
+        get
+        {
+            return swipeDown;
+        }
+    }
+#endregion
+}
